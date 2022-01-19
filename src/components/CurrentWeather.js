f6d39f1e9c985f8181a62e4303e3902e75a9f605
Fernouny <65189013+devferno@users.noreply.github.com>
2022-01-19 23:57:24 +0100

    Update CurrentWeather.js

--- src/components/CurrentWeather.js
+++ src/components/CurrentWeather.js
@@ -41,7 +41,6 @@ const CurrentWeather = () => {
   if (isFetching) {
     return <p>...loading</p>;
   }
-  console.log(data);
   const { main, visibility, wind, weather } = data;
   return (
     <Paper
